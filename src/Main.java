c9b779486b5a6d7d871f7d9b3d7ffd929bc15f3b
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-19 16:36:15 +0100

    Solved

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,19 @@ public class Main {
     public static void main(String[] args) {
         // Lägg till en metod i klassen Punkt som räknar ut avståndet till origo.
 
+        Punkt p = new Punkt();
+        p.x = 5;
+        p.y = 3;
 
+        System.out.println("Avstånd till origo: " + p.distance());
     }
 }
 
 class Punkt {
     double x;
     double y;
+
+    double distance() {
+        return Math.sqrt(x*x + y*y);
+    }
 }
