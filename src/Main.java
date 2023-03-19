12a50f12cd93471bca03473ce8690fc6b1697148
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-19 16:30:31 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Lägg till en metod i klassen Punkt som räknar ut avståndet till origo.
+
+
+    }
+}
+
+class Punkt {
+    double x;
+    double y;
+}
